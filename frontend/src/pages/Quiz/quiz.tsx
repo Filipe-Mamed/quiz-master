4f57a15d9d05039027e1f914b9c3d58af2e3e719
Filipe-Mamed <filipemamed9@gmail.com>
2025-11-23 00:27:26 -0300

    feat: Add a timer and improve the quiz layout

--- frontend/src/pages/Quiz/quiz.tsx
+++ frontend/src/pages/Quiz/quiz.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { RxArrowLeft, RxArrowRight } from "react-icons/rx";
 
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react";
 
 import { Spinner } from "@/shared/components/Spinner";
 import { Card } from "@/shared/components/Card";
@@ -10,6 +10,7 @@ import { api } from "@/shared/service/backend-api";
 import { type I_Question } from "@/shared/types";
 import { Toast } from "@/shared/components/Toast";
 import { getErrorMessage } from "@/shared/utils/get-error-message";
+import { Stopwatch } from "@/shared/components/Stopwatch";
 
 // Componente principal do Quiz
 export const Quiz = () => {
@@ -18,7 +19,7 @@ export const Quiz = () => {
   // Índice da pergunta atual
   const [currentIndex, setCurrentIndex] = useState(0);
   // Respostas do usuário (índice da alternativa escolhida ou null)
-  const [answers, setAnswers] = useState<(number | null)[]>([]);
+  const [answers, setAnswers] = useState<number[]>([]);
   // Indica se os dados ainda estão sendo carregados
   const [loading, setLoading] = useState(true);
 
@@ -121,7 +122,7 @@ export const Quiz = () => {
     setAnswers((prev) => {
       const newAnswers = [...prev]; // Alterna seleção: se já estava selecionada, limpa
       newAnswers[currentIndex] =
-        newAnswers[currentIndex] === altIndex ? null : altIndex;
+        newAnswers[currentIndex] === altIndex ? -1 : altIndex;
       return newAnswers;
     });
   };
@@ -132,93 +133,130 @@ export const Quiz = () => {
         // Exibe spinner enquanto carrega perguntas
         <Spinner />
       ) : (
-        <Card>
-          <Box mb={4}>
-            <Box mb={4}>
-              <Heading>
-                {/* Exibe enunciado da pergunta atual */}
-                {currentIndex + 1}. {currentQuestion.question}
-              </Heading>
-            </Box>
-            {/* Renderiza alternativas */}
-            {currentQuestion.alternatives.map((alt, i) => (
-              <Button
-                key={i}
-                justifyContent="flex-start"
-                variant="outline"
-                p={3}
-                w="100%"
-                border="1px solid #ccc"
-                borderRadius="md"
-                bg={answers[currentIndex] === i ? "color.blue" : "transparent"}
-                color={answers[currentIndex] === i ? "color.white" : "black"}
-                _hover={{
-                  bg: "gray.300",
-                }}
-                h="50px"
-                mt={3}
-                onClick={() => handleSelect(i)}
-              >
-                ({String.fromCharCode(65 + i)}) {alt}
-              </Button>
-            ))}
+        <Stack gap={5} minH="100vh" align="center" justify="center">
+          <Box position="relative" width="100%" maxW="600px">
+            <Card
+              position="absolute"
+              top={-10}
+              right={1}
+              bg="bg.card"
+              rounded="xl"
+              shadow="button"
+              p={1}
+              maxW="70px"
+              w="100%"
+              textAlign="center"
+            >
+              <Stopwatch
+                userResponse={questions.map((q, i) => {
+                  return {
+                    id: q.id,
+                    answer: answers[i],
+                  };
+                })}
+              />
+            </Card>
+            <Card
+              bg="bg.card"
+              rounded="xl"
+              shadow="button"
+              p={5}
+              maxW="600px"
+              w="100%"
+            >
+              {/* Exibe cronômetro regressivo para concluir o quiz, redirecionando automaticamente para o resultado ao final */}
+              <Box mb={4}>
+                <Box mb={4}>
+                  <Heading>
+                    {/* Exibe enunciado da pergunta atual */}
+                    {currentIndex + 1}. {currentQuestion.question}
+                  </Heading>
+                </Box>
+                {/* Renderiza alternativas */}
+                {currentQuestion.alternatives.map((alt, i) => (
+                  <Button
+                    key={i}
+                    justifyContent="flex-start"
+                    variant="outline"
+                    p={3}
+                    w="100%"
+                    border="1px solid #ccc"
+                    borderRadius="md"
+                    bg={
+                      answers[currentIndex] === i ? "color.blue" : "transparent"
+                    }
+                    color={
+                      answers[currentIndex] === i ? "color.white" : "black"
+                    }
+                    _hover={{
+                      bg: "gray.300",
+                    }}
+                    h="50px"
+                    mt={3}
+                    onClick={() => handleSelect(i)}
+                  >
+                    ({String.fromCharCode(65 + i)}) {alt}
+                  </Button>
+                ))}
+              </Box>
+
+              <Box>
+                {/* Mostra progresso do quiz */}
+                <Text color="gray" mb={4} textAlign="center">
+                  Pergunta {currentIndex + 1} de {questions.length}
+                </Text>
+
+                {/* Botões de navegação (anterior/próxima) */}
+                <Box display="flex" gap={4} justifyContent="space-between">
+                  <Button
+                    onClick={prevQuestion}
+                    disabled={currentIndex === 0}
+                    variant="outline"
+                    color="blue"
+                    borderColor="color.blue"
+                    rounded="4xl"
+                    _hover={{
+                      boxShadow: "buttonHover",
+                      bg: "color.blue",
+                      opacity: "hover",
+                      color: "white",
+                    }}
+                    _active={{
+                      opacity: "active",
+                      boxShadow: "buttonHover",
+                      bg: "color.primary",
+                      color: "white",
+                    }}
+                  >
+                    <RxArrowLeft />
+                  </Button>
+
+                  <Button
+                    onClick={nextQuestion}
+                    disabled={isNextButtonDisabled}
+                    bg="color.blue"
+                    color="white"
+                    rounded="4xl"
+                    _hover={{
+                      boxShadow: "buttonHover",
+                      bg: "color.blue",
+                      opacity: "hover",
+                      color: "white",
+                    }}
+                    _active={{
+                      opacity: "active",
+                      boxShadow: "buttonHover",
+                      bg: "color.primary",
+                      color: "white",
+                    }}
+                  >
+                    {nextButtonText}
+                  </Button>
+                </Box>
+              </Box>
+            </Card>
           </Box>
-
-          <Box>
-            {/* Mostra progresso do quiz */}
-            <Text color="gray" mb={4} textAlign="center">
-              Pergunta {currentIndex + 1} de {questions.length}
-            </Text>
-
-            {/* Botões de navegação (anterior/próxima) */}
-            <Box display="flex" gap={4} justifyContent="space-between">
-              <Button
-                onClick={prevQuestion}
-                disabled={currentIndex === 0}
-                variant="outline"
-                color="blue"
-                borderColor="color.blue"
-                rounded="4xl"
-                _hover={{
-                  boxShadow: "buttonHover",
-                  bg: "color.blue",
-                  opacity: "hover",
-                  color: "white",
-                }}
-                _active={{
-                  opacity: "active",
-                  boxShadow: "buttonHover",
-                  bg: "color.primary",
-                  color: "white",
-                }}
-              >
-                <RxArrowLeft />
-              </Button>
-
-              <Button
-                onClick={nextQuestion}
-                disabled={isNextButtonDisabled}
-                bg="color.blue"
-                color="white"
-                rounded="4xl"
-                _hover={{
-                  boxShadow: "buttonHover",
-                  bg: "color.blue",
-                  opacity: "hover",
-                  color: "white",
-                }}
-                _active={{
-                  opacity: "active",
-                  boxShadow: "buttonHover",
-                  bg: "color.primary",
-                  color: "white",
-                }}
-              >
-                {nextButtonText}
-              </Button>
-            </Box>
-          </Box>
-        </Card>
+        </Stack>
       )}
     </>
   );
